44d9944139aa5c0c0b228c0eb48ce51401a80fa7
whiteSkar <sypower07@hotmail.com>
2016-11-05 20:44:33 -0700

    veeeeery basic client and server communication using login

---
+++ Experimental/Assets/Code/Manager/AuthManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Text;
+using System.IO;
+
+public class AuthManager : MonoBehaviour
+{
+
+	void Start ()
+    {
+        var request = (HttpWebRequest)WebRequest.Create("http://localhost:8000/coconut/create_user/");
+
+        var postData = "user_name=test";
+        var data = Encoding.ASCII.GetBytes(postData);
+
+        request.Method = "POST";
+        request.ContentType = "application/x-www-form-urlencoded";
+        request.ContentLength = data.Length;
+
+        using (var stream = request.GetRequestStream())
+        {
+            stream.Write(data, 0, data.Length);
+        }
+
+        Debug.LogError("Sending request");
+        var response = (HttpWebResponse)request.GetResponse();
+
+        var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        Debug.LogError(responseString);
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
