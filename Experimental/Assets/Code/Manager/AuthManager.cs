24c593df0133116763524c938c7d400ada5429d8
whiteSkar <sypower07@hotmail.com>
2016-11-11 17:39:16 -0800

    add very basic user login

--- Experimental/Assets/Code/Manager/AuthManager.cs
+++ Experimental/Assets/Code/Manager/AuthManager.cs
@@ -2,15 +2,27 @@
 using System.Net;
 using System.Text;
 using System.IO;
+using SimpleJSON;
 
 public class AuthManager : MonoBehaviour
 {
-
-	void Start ()
+    enum ResponseStatus
     {
-        var request = (HttpWebRequest)WebRequest.Create("http://localhost:8000/coconut/create_user/");
+        SUCCESS = 1,
+        FAIL = 2,
+        INVALID_ARGUMENT = 3,
+        CREATE_USER = 4,
+        USER_WITH_SAME_DEVICE_ID_ALREADY_EXIST = 5,
+        USER_WITH_SAME_NAME_ALREADY_EXIST = 6,
+    }
 
-        var postData = "user_name=test";
+    void Start()
+    {
+        var request = (HttpWebRequest)WebRequest.Create("http://localhost:8000/coconut/authenticate_user/");
+
+        var device_id = SystemInfo.deviceUniqueIdentifier;
+        Debug.LogError("device_id=" + device_id);
+        var postData = "device_id=" + device_id;
         var data = Encoding.ASCII.GetBytes(postData);
 
         request.Method = "POST";
@@ -26,11 +38,66 @@ public class AuthManager : MonoBehaviour
         var response = (HttpWebResponse)request.GetResponse();
 
         var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-        Debug.LogError(responseString);
-	}
+        var responseJson = JSON.Parse(responseString);
+        Debug.LogError(responseJson);
+
+        ResponseStatus responseStatus = (ResponseStatus)responseJson["status"].AsInt;
+        switch (responseStatus)
+        {
+            case ResponseStatus.SUCCESS:
+                Debug.LogError(responseStatus.ToString() + ": " + responseJson["user"].Value);
+                break;
+            case ResponseStatus.FAIL:
+            case ResponseStatus.INVALID_ARGUMENT:
+                Debug.LogError(responseStatus.ToString() + ": " + responseJson["msg"].Value);
+                break;
+            case ResponseStatus.CREATE_USER:
+                make_create_user_request(device_id, "whiteSkar"); //TODO: get from UI
+                break;
+            default:
+                Debug.LogError("Unhandled response status" + responseStatus.ToString());
+                break;
+        }
+    }
 	
-	void Update ()
+	void make_create_user_request(string device_id, string user_name)
     {
-	
-	}
+        var request = (HttpWebRequest)WebRequest.Create("http://localhost:8000/coconut/create_user/");
+
+        var postData = "device_id=" + device_id + "&user_name=" + user_name;
+        var data = Encoding.ASCII.GetBytes(postData);
+
+        request.Method = "POST";
+        request.ContentType = "application/x-www-form-urlencoded";
+        request.ContentLength = data.Length;
+
+        using (var stream = request.GetRequestStream())
+        {
+            stream.Write(data, 0, data.Length);
+        }
+
+        Debug.LogError("Sending request");
+        var response = (HttpWebResponse)request.GetResponse();
+
+        var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        var responseJson = JSON.Parse(responseString);
+        Debug.LogError(responseJson);
+
+        ResponseStatus responseStatus = (ResponseStatus) responseJson["status"].AsInt;
+        switch (responseStatus)
+        {
+            case ResponseStatus.SUCCESS:
+            case ResponseStatus.USER_WITH_SAME_DEVICE_ID_ALREADY_EXIST:
+                Debug.LogError(responseStatus.ToString() + ": " + responseJson["user"].Value);
+                break;
+            case ResponseStatus.FAIL:
+            case ResponseStatus.INVALID_ARGUMENT:
+            case ResponseStatus.USER_WITH_SAME_NAME_ALREADY_EXIST:
+                Debug.LogError(responseStatus.ToString() + ": " + responseJson["msg"].Value);
+                break;
+            default:
+                Debug.LogError("Unhandled response status" + responseStatus.ToString());
+                break;
+        }
+    }
 }
